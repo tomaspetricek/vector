2babfe4ca6089ed0f9a34219e78eb67beba2f4b2
TomasPetricek <45512445+tomaspetricek@users.noreply.github.com>
2022-09-22 12:11:15 +0200

    Implement vector class

--- vector.h
+++ vector.h
@@ -5,27 +5,77 @@
 
 namespace epc
 {
-    template <typename T>
-    class vector
-    {
+    template<typename T>
+    class vector {
+        std::size_t capacity_{0};
+        std::size_t size_{0};
+        T* data_{nullptr};
+
     public:
-        vector() noexcept { }        
+        vector() noexcept = default;
 
         vector(const vector&) = delete;
         vector& operator=(const vector&) = delete;
 
-        ~vector() { }
+        ~vector()
+        {
+            if (capacity_) delete[] data_;
+        }
 
-        T* data() { }
-        const T* data() const { }
+        T* data()
+        {
+            return data_;
+        }
 
-        T& operator[](size_t) { }
-        const T& operator[](size_t) const { }
+        const T* data() const
+        {
+            return data_;
+        }
 
-        void push_back(const T&) { }
+        T& operator[](size_t i)
+        {
+            return data_[i];
+        }
 
-        size_t capacity() const { }
-        size_t size() const { }
+        // check if correct
+        const T& operator[](size_t i) const
+        {
+            return data_[i];
+        }
+
+        void push_back(const T& el)
+        {
+            if (size_==capacity_) {
+                // increase capacity
+                capacity_ = (!capacity_) ? 1 : capacity_*2;
+
+                // allocate new array
+                T* temp = new T[capacity_];
+
+                // copy elements
+                for (size_t i{0}; i<size_; i++)
+                    temp[i] = data_[i];
+
+                // deallocate old array
+                delete[] data_;
+
+                // assign new array
+                data_ = temp;
+            }
+
+            // add last element
+            data_[size_++] = el;
+        }
+
+        size_t capacity() const
+        {
+            return capacity_;
+        }
+
+        size_t size() const
+        {
+            return size_;
+        }
     };
 }
 
