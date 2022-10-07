8d8df6b62ad57e62e3368d63695d225385faf414
TomasPetricek <45512445+tomaspetricek@users.noreply.github.com>
2022-10-07 09:09:19 +0200

    Implement vector

--- vector.h
+++ vector.h
@@ -2,38 +2,122 @@
 #define EPC_VECTOR
 
 #include <cstdlib>
+#include <algorithm>
 
-namespace epc
-{
-   template <typename T>
-   class vector
-   {
-      public:
-         vector() noexcept { }
+namespace epc {
+    template<typename T>
+    class vector {
+        std::size_t capacity_{0};
+        std::size_t size_{0};
+        T* data_{nullptr};
 
-         vector(const vector&) { }
+    public:
+        vector() noexcept = default;
 
-         vector& operator=(const vector&) { }
+        vector(const vector& src)
+                :capacity_{src.size_}, size_{src.size_}
+        {
+            try {
+                data_ = size_ ? new T[size_] : nullptr;
+                std::copy(src.data_, src.data_+src.size_, data_);
+            }
+            catch (...) {
+                delete[] data_;
+                throw;
+            }
+        }
 
-         ~vector() { }
+        vector& operator=(const vector& rhs)
+        {
+            if (&rhs!=this) {
+                // copy-and-swap idiom
+                vector temp(rhs);
+                swap(temp);
+            }
+            return *this;
+        }
 
-         T* data() { }
-         const T* data() const { }
+        ~vector()
+        {
+            if (capacity_) delete[] data_;
+        }
 
-         T& operator[](size_t) { }
-         const T& operator[](size_t) const { }
+        T* data()
+        {
+            return data_;
+        }
 
-         void push_back(const T&) { }
+        const T* data() const
+        {
+            return data_;
+        }
 
-         void swap(vector& other) noexcept { }
+        T& operator[](size_t i)
+        {
+            return data_[i];
+        }
 
-         size_t capacity() const { }
-         size_t size() const { }
+        const T& operator[](size_t i) const
+        {
+            return data_[i];
+        }
 
-         void reserve(size_t) { }
+        void push_back(const T& el)
+        {
+            if (size_==capacity_)
+                reserve((!capacity_) ? 1 : capacity_*2);
 
-         void pop_back() { } 
-   };
+            try {
+                data_[size_++] = el;
+            }
+            catch (...) {
+                size_--;
+                throw;
+            }
+        }
+
+        size_t capacity() const
+        {
+            return capacity_;
+        }
+
+        size_t size() const
+        {
+            return size_;
+        }
+
+        void swap(vector& other) noexcept
+        {
+            std::swap(data_, other.data_);
+            std::swap(capacity_, other.capacity_);
+            std::swap(size_, other.size_);
+        }
+
+        void reserve(size_t capacity)
+        {
+            if (capacity>capacity_) {
+                T* temp{nullptr};
+
+                try {
+                    temp = new T[capacity];
+                    std::copy(data_, data_+size_, temp);
+                }
+                catch (...) {
+                    delete[] temp;
+                    throw;
+                }
+
+                delete[] data_;
+                data_ = temp;
+                capacity_ = capacity;
+            }
+        }
+
+        void pop_back()
+        {
+            if (size_) size_--;
+        }
+    };
 }
 
 #endif
